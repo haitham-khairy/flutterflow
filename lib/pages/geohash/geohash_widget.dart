89aa344e2794d855aa3ac23a9fbc07c06269b9e7
FlutterFlow <140657486+FlutterFlowEng@users.noreply.github.com>
2024-04-17 19:28:33 +0000

    Updating to latest FlutterFlow output.

--- lib/pages/geohash/geohash_widget.dart
+++ lib/pages/geohash/geohash_widget.dart
@@ -36,7 +36,7 @@ class _GeohashWidgetState extends State<GeohashWidget> {
       _model.instantTimer = InstantTimer.periodic(
         duration: const Duration(milliseconds: 3000),
         callback: (timer) async {
-          if (_model.rfidstatus != 'connection complete') {
+          if (_model.rfidstatus == 'connection complete') {
             _model.rfidreaderfeedback = await actions.getRFIDReaderStatus();
             setState(() {
               _model.rfidstatus = _model.rfidreaderfeedback!;
